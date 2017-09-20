cc72172eefe094cc46ae28f8382ef92844af4e1a
Samy Yahia <samy.yahia@hetic.net>
2017-09-21 00:26:05 +0200

    Update DefaultController.php

--- src/PhoneBundle/Controller/DefaultController.php
+++ src/PhoneBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('PhoneBundle:Default:index.html.twig');
+        return $this->render('PhoneBundle:Pages:home.html.twig');
     }
 
 }
