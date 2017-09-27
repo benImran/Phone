5d816735de6d6eae37a30faad557f828180cb197
LeaCharrier <lea.charrier@hetic.net>
2017-09-27 21:32:22 +0200

    Commande en commentaire
    npm install jquery
    bower install jquery
    bower install https://code.jquery.com/jquery-3.2.1.min.js

--- web/js/main.js
+++ web/js/main.js
@@ -3,27 +3,16 @@ $(document).ready(function() {
 });
 
 var toggleInscriptionConnexion = function() {
-
-    // $('.connect .title h2').on('click', function() {
-    //   $(this).toggleClass('active');
-    //   var titleAttr = $(this).attr('data-content');
-    //   var contentAttr = $('.content[data-content'+ titleAttr +']').toggleClass('active');
-    //   console.log(titleAttr);
-    //   console.log(contentAttr);
-
-    //   if (jQuery('.connect .title h2').hasClass('active') && jQuery('.connect .content').hasClass('active')) {
-    //     jQuery('.connect .title h2').removeClass('active');
-    //     jQuery('.connect .content').removeClass('active');
-    //     jQuery(this).toggleClass('active');
-    //   }
-    // });
-    var titleAttr = $('.connect .title h2').attr('data-content');
-    console.log(titleAttr);
-    var contentAttr = $('.content[data-content='+ titleAttr +']');
-    console.log(contentAttr);
-
-
-    $('.connect').on('click', function() {
-      console.log(titleAttr);
-    });
+  $('.connect .title h2').on('click', function() {
+    var titleAttr = $(this).attr('data-content');
+    var contentAttr = $('.content[data-content="' + titleAttr + '"]').addClass('active');
+    if (jQuery('.connect .title h2').hasClass('active') || jQuery('.connect .content').hasClass('active')) {
+      jQuery('.connect .title h2').toggleClass('active', false);
+      jQuery('.connect .content').toggleClass('active', false);
+      $(this).toggleClass('active', true);
+      contentAttr.toggleClass('active', true);
+    } else {
+      $(this).toggleClass('active');
+    }
+  });
 };
